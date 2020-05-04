4eb8738445e89b81fe7409b4133af817be17168e
EC2 Default User <ec2-user@ip-172-31-90-148.ec2.internal>
2020-05-04 21:51:23 +0000

    added distiniction for http verbs

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 var express = require('express');
 
 var app = express();
 
 app.get('/', function(req, res) {
   res.send({
-    "Output": "Hello Get World!"
+    "Output": "Hello Get Request!"
   });
 });
 
 app.post('/', function(req, res) {
   res.send({
-    "Output": "Hello Post World!"
+    "Output": "Hello Post Request!"
   });
 });
 
